fb7483396e776100f2258a659426d10c107a722a
persistenctly <3285576618@qq.com>
2022-06-08 18:49:05 +0800

    完成了商品列表页面的开发

--- App.vue
+++ App.vue
@@ -1,6 +1,6 @@
 <script>
 	export default {
-		
+
 		onLaunch: function() {
 			console.warn('当前组件仅支持 uni_modules 目录结构 ，请升级 HBuilderX 到 3.1.0 版本以上！')
 			console.log('App Launch')
@@ -19,6 +19,7 @@
 	@import '@/uni_modules/uni-scss/index.scss';
 	/* #ifndef APP-NVUE */
 	@import '@/static/customicons.css';
+
 	// 设置整个项目的背景色
 	page {
 		background-color: #f5f5f5;
@@ -30,29 +31,36 @@
 		color: #333;
 		padding: 10px;
 	}
-	
-	
+
+
 	//自定义 search 组件的样式
 	.my-search-container {
 		// 移除背景颜色，改由 props 属性控制
-		// background-color: #C00000;
+		// background-color: #C00000;
+
 		height: 50px;
 		padding: 0 10px;
 		display: flex;
 		align-items: center;
 	}
+
 	.my-search-box {
 		height: 36px;
 		background-color: #ffffff;
-	// 移除圆角尺寸，改由 props 属性控制
-	// border-radius: 15px;
+		// 移除圆角尺寸，改由 props 属性控制
+		// border-radius: 15px;
+
 		width: 100%;
 		display: flex;
 		align-items: center;
 		justify-content: center;
+
 		.placeholder {
-		font-size: 15px;
-		margin-left: 5px;
+			font-size: 15px;
+			margin-left: 5px;
 		}
 	}
+	
+	
+
 </style>
